be07d2ea7c999698517b66fc3513b94a076ca66e
niciyan <nishiyamasu@gmail.com>
2024-05-04 01:56:30 +0000

    add components

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Stack, Button, IconButton } from '@mui/material'
+import SendIcon from '@mui/icons-material/Send';
+
+const MuiButton = () => {
+  return (
+    <Stack spacing={4}>
+        <Stack spacing={2} direction='row'>
+            <Button variant='text' href='https://google.com'>Text</Button>        
+            <Button variant='contained'>contained</Button>        
+            <Button variant='outlined'>OutLined</Button>        
+        </Stack>
+        <Stack spacing={2} direction='row'>
+            <Button variant='contained' color='primary'>Primary</Button>
+            <Button variant='contained' color='secondary'>Secondary</Button>
+            <Button variant='contained' color='error'>Error</Button>
+            <Button variant='contained' color='warning'>Warning</Button>
+        </Stack>
+        <Stack display='block' spacing={2} direction='row'>
+            <Button variant='contained' size='small'>Small</Button>
+            <Button variant='contained' size='medium'>Medium</Button>
+            <Button variant='contained' size='large'>Large</Button>
+        </Stack>
+        <Stack spacing={2} direction='row'>
+            <Button variant='contained' startIcon={<SendIcon />} disableRipple onClick={() => alert('Clicked')} >Send</Button>
+            <Button variant='contained' endIcon={<SendIcon />} disableElevation >Send</Button>
+            <IconButton aria-label='send' color='success' size='small'>
+                <SendIcon/>
+            </IconButton>
+        </Stack>
+    </Stack>
+
+  )
+}
+
+export default MuiButton
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,24 @@
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant="h1">h1 heading</Typography>
+            <Typography variant="h2">h2 heading</Typography>
+            <Typography variant="h3">h3 heading</Typography>
+            <Typography variant="h4">h4 heading</Typography>
+            <Typography variant="h5">h5 heading</Typography>
+
+
+            <Typography variant="subtitle1">Sub title 1</Typography>
+            <Typography variant="subtitle2">Sub title 2</Typography>
+
+            <Typography variant="body1">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat aliquam nisi quis tenetur iure dolores cumque a nostrum? Nisi consequuntur quia velit nesciunt natus, ex itaque vero beatae nulla dignissimos?
+            </Typography>
+            <Typography variant="body2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere odio veritatis ea optio sunt, distinctio fugiat. Vel iusto quod dignissimos, beatae saepe reiciendis, perferendis laudantium repellendus animi, provident dolor asperiores!
+            </Typography>
+        </div>
+    )
+}
